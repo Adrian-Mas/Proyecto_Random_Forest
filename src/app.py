287db92a59c4b9963b4d6303980783d0185cd2b7
Adrian-Mas <adrian.moure.asorey@gmail.com>
2024-12-16 20:15:26 +0000

    FIN

--- src/app.py
+++ src/app.py
@@ -2,3 +2,10 @@ from utils import db_connect
 engine = db_connect()
 
 # your code here
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "Hello, World!"
